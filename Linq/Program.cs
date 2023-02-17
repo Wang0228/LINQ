3440db0c6ccd9cadf787fb876bcf1e60c624bbd0
王子豪 <as258652000@yahoo.com>
2023-02-17 15:26:39 +0800

    test

--- Linq/Program.cs
+++ Linq/Program.cs
@@ -16,17 +16,37 @@ namespace Linq
             foreach(var item in list)Console.WriteLine(item.ProductNumber);
             //1. 計算所有商品的總價格
             int sum =list.Select(item => item.Price).Sum();
-            Console.WriteLine($"所有商品總價格: {sum:N}");
+            Console.WriteLine($"所有商品總價格: {sum:C}");
             //2. 計算所有商品的平均價格
             decimal avg=list.Select(item => Convert.ToDecimal(item.Price)).Average();
-            Console.WriteLine($"所有商品的平均價格: {avg:N2}");
+            Console.WriteLine($"所有商品的平均價格: {avg:C2}");
             //3. 計算商品的總數量
             int count=list.Count();
-            Console.WriteLine($"商品總數量:{count}");
+            Console.WriteLine($"商品總數量: {count}");
             //4. 計算商品的平均數量
             var avgQuantity=list.Select(item =>item.ProductQuantity).Average();
             Console.WriteLine($"商品的平均數量: {avgQuantity:N}");
+            //5. 找出哪一項商品最貴
+            var expensive_find = list.Max(x => x.Price) ;
+            var expensive = list.FirstOrDefault(x=>x.Price==expensive_find);
+            Console.WriteLine($"最貴的商品: {expensive.ProductName}");
+            //6. 找出哪一項商品最便宜
+            var cheap_find=list.Min(x=> x.Price) ;
+            var cheap=list.FirstOrDefault(x=>x.Price==cheap_find);
+            Console.WriteLine($"最便宜的商品: {cheap.ProductName}");
+            //7. 計算產品類別為 3C 的商品總價
+            var price_ALL_3C_find = list.Where (x => x.ProductCategory == "3C").Sum(x=>x.Price);
+            //foreach(var item in price_ALL_3C_find) 
+            //{ 
+            //    Console.WriteLine($"{item.ProductCategory == "3C"}"); 
+            //}
+            //var price_ALL_3C = price_ALL_3C_find.Sum(x => x.Price);
+            Console.WriteLine($"產品類別為3C的商品總價: {price_ALL_3C_find}");
 
+            foreach (var item in list)
+            {
+                Console.WriteLine($"{item.ProductName}-{item.ProductNumber}-{item.Price}-{item.ProductQuantity}-{item.ProductCategory}");
+            }
 
             Console.ReadLine();
         }
